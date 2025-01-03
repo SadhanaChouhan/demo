e2af84d48e38165f338549685615cc597451a212
Sadhana Chouhan <chouhansadhana17@gmail.com>
2025-01-03 14:19:59 +0530

    add result page

---
+++ script.js
@@ -0,0 +1,9 @@
+// selecting all elements
+
+const main_page = document.querySelector(".main-page");
+const  start_option = main_page.querySelector(".option");
+ const quiz_box = document.querySelector(".quiz-box");
+ const result_box = document.querySelector(".result-box");
+ const options = document.querySelector(".options");
+  
+//  main_page.classList.add("activeMainPage");
